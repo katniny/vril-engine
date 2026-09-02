ac7516bcdcd3039b9f65a45fa4e4adaf8b81d212
katniny <okuwu1680@gmail.com>
2026-09-01 23:25:23 -0600

    SDL: Copy DLLs over

--- testing/setup/windows.sh
+++ testing/setup/windows.sh
@@ -34,6 +34,7 @@ function obtain_nzportable()
 	fi
 
 	cp "${binary_path}" "${working_dir}/nzportable/${APP_BIN}"
+	cp "$(dirname "${binary_path}")"/*.dll "${working_dir}/nzportable/"
 	chmod +x "${working_dir}/nzportable/${APP_BIN}"
 }
 
